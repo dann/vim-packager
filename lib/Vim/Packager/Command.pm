9f2770eba72e242720b51d4c215e80691986ebee
c9s <cornelius.howl@gmail.com>
2009-10-24 03:11:51 +0800

    implement invoke function

--- lib/Vim/Packager/Command.pm
+++ lib/Vim/Packager/Command.pm
@@ -4,6 +4,13 @@ use strict;
 use base qw(App::CLI App::CLI::Command);
 use Getopt::Long qw(:config no_ignore_case bundling);
 
-
+$|++;
+sub invoke {
+    my ($pkg,$cmd,@args)=@_;
+    local *ARGV = [ $cmd , @args ];
+    my $ret = eval { $pkg->dispatch() };
+    warn $@ if $@;
+    return ($ret || 0);
+}
 
 1;
