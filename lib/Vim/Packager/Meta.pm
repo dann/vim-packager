1031a220fddca5d8571c160f445ee6e1b80dbbc1
c9s <cornelius.howl@gmail.com>
2009-10-24 04:57:21 +0800

    add meta info accessor

--- lib/Vim/Packager/Meta.pm
+++ lib/Vim/Packager/Meta.pm
@@ -2,12 +2,20 @@ package Vim::Packager::Meta;
 use warnings;
 use strict;
 
+use YAML;
+
 my @possible_filename = qw( 
     META
     MEAT.yml
 );
 
+
 sub new { bless {} , shift }
+
+sub meta {
+    my $self = shift;
+    return $self->{meta} ||= {};
+}
 
 sub get_meta_file {
     for my $f ( @possible_filename ) {
@@ -36,7 +44,7 @@ sub get_meta_file {
 =cut
 
 sub read {
-    my $new = shift;
+    my $class = shift;
     my $file = $class->get_meta_file();
 
     die 'there is no meta file' unless -e $file;
@@ -55,17 +63,51 @@ sub read {
     }
 }
 
+sub convert_to_yaml {
+
+}
+
+sub _get_value {
+    my $cur = shift;
+    my ($v) = ( $cur =~ /^=\w+ (.*)$/ ) ;
+    return $v;
+}
+
 sub _name {
+    my ($self,$cur,$lines,$idx) = @_;
+    $self->meta->{name} = _get_value( $cur );
+}
+
+sub _author {
+    my ($self,$cur,$lines,$idx) = @_;
+    $self->meta->{author} = _get_value( $cur );
+}
+
+sub _version {
+    my ($self,$cur,$lines,$idx) = @_;
+    $self->meta->{version} = _get_value( $cur );
+}
+
+sub _type {
+    my ($self,$cur,$lines,$idx) = @_;
+    $self->meta->{type} = _get_value( $cur );
+}
+
+sub _dependency {
+    my ($self,$cur,$lines,$idx) = @_;
+
+
+}
+
+sub _script {
     my ($self,$cur,$lines,$idx) = @_;
 
 }
 
-sub _author { }
-sub _version { }
-sub _type { }
-sub _dependency { }
-sub _script { }
-sub _repository { }
+sub _repository {
+    my ($self,$cur,$lines,$idx) = @_;
+
+}
 
 # some alias
 
