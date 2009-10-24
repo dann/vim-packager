82544380f6403203ec524c420a2c50dd35cb27db
c9s <cornelius.howl@gmail.com>
2009-10-25 00:48:21 +0800

    implmenet manifest reader

--- lib/Vim/Packager/Command/Build.pm
+++ lib/Vim/Packager/Command/Build.pm
@@ -17,13 +17,24 @@ sub run {
     my $meta_reader = Vim::Packager::MetaReader->new;
 
     my $file = $meta_reader->get_meta_file();
-    die 'there is no meta_reader file' unless -e $file;
+    die 'Can not found META file' unless -e $file;
 
     open my $fh , "<" , $file ;
     $meta_reader->read( $fh );
     close $fh;
 
     YAML::DumpFile( "META.yml" , $meta_reader->meta );
+}
+
+
+sub read_manifest {
+    my $self = shift;
+    my $file = 'MANIFEST';
+    open my $fh , "<" , $file or die "Can not found MANIFEST";
+    my @files = <$fh>;
+    chomp @files;
+    close $fh;
+    return \@files;
 }
 
 1;
