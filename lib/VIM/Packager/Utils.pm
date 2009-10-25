6445e75c9c8399b06c18f552bb645fa2672ae8a2
c9s <cornelius.howl@gmail.com>
2009-10-26 03:09:44 +0800

    ok , now it's 'VIM'

---
+++ lib/VIM/Packager/Utils.pm
@@ -0,0 +1,17 @@
+package VIM::Packager::Utils;
+use warnings;
+use strict;
+
+
+sub findbin {
+    my $which = shift;
+    my $path  = $ENV{PATH};
+    my @paths = split /:/, $path;
+    for (@paths) {
+        my $bin = $_ . '/' . $which;
+        return $bin if ( -x $bin );
+    }
+}
+
+
+1;
